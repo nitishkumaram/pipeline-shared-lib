b73698e33169efb01f84463ba36262a40c5eb059
nitishkumaram <nitishkumar@outlook.com>
2019-09-29 23:00:54 +0530

    Fixed minor issue

--- src/com/acceleratedskillup/validatorUtils.groovy
+++ src/com/acceleratedskillup/validatorUtils.groovy
@@ -1,6 +1,7 @@
-package com.acceleratedskillup
+package com.acceleratedskillup;
 
 class ValidatorUtils{
-    static def validateInput(script)
-    script.echo "In Static validateInput()"
+    static def validateInput(script) {
+        script.echo "In Static validateInput()"
+    }
 }
