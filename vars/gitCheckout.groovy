6486f85999f3c1107b5528e564febf65fd6a52e2
nitishkumaram <nitishkumar@outlook.com>
2019-10-14 23:21:42 +0530

    Added reference URL

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -1,3 +1,5 @@
 def call(Map stageParams) {
     checkout([$class: 'GitSCM', branches: [[name: stageParams.branch]], userRemoteConfigs: [[url: stageParams.url]]])
 }
+
+// Reference: https://devopscube.com/create-jenkins-shared-library/
